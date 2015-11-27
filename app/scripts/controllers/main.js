7c40037ef7dd656c70a66b375f54abc748686b13
Josh Woodward <jdw6359@rit.edu>
2015-11-26 21:50:09 -0500

    Initial Application Development
    Scaffold angular application
    Add gulp resources

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name seadvisingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the seadvisingApp
+ */
+angular.module('seadvisingApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
