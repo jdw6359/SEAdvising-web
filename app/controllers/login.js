6f9a1a89fb2963e78b20f8421b28517ed5514a28
Josh Woodward <jdw6359@rit.edu>
2015-12-10 10:42:16 -0500

    Login Template Created, Hooked to Login Directive

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,6 +3,10 @@
 LoginController.$inject = ['$scope'];
 function LoginController($scope){
 	console.log("login controller");
+
+	this.login = function(){
+		console.log("Login Button Pressed");
+	}
 }
 
 module.exports = LoginController;
