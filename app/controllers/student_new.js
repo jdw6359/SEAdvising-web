ea773680173512be0b0645c49ed655ac5bec034f
Josh Woodward <jdw6359@rit.edu>
2015-12-16 20:42:16 -0500

    Create new student added

--- app/controllers/student_new.js
+++ app/controllers/student_new.js
@@ -1,11 +1,15 @@
 'use-strict'
 
-StudentNewController.$inject = ['StudentFactory', 'AdvisorFactory'];
+StudentNewController.$inject = ['$http', '$scope', '$location', 'StudentFactory', 'AdvisorFactory'];
 
-function StudentNewController(StudentFactory, AdvisorFactory){
+function StudentNewController($http, $scope, $location, StudentFactory, AdvisorFactory){
+
+	//TODO: Move this to request interceptor
+    $http.defaults.headers.common["Auth-Token"] = $scope.authToken;
 
 	var vm = this;
 
+	vm.student = {};
 	vm.advisors = AdvisorFactory.query();
 
 
@@ -14,9 +18,14 @@ function StudentNewController(StudentFactory, AdvisorFactory){
 		//TODO: add form validation here
 		console.log("new student submit clicked...student:");
 		console.log(vm.student);
+
+		StudentFactory.save({student: vm.student}, function(res){
+			$location.path('/students');
+		}, function(err){
+			console.log("error response");
+			console.log(err);
+		});
 	}
-
-
 }
 
 module.exports = StudentNewController;
