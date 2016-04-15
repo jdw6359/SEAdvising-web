d8b978b82e41207501febfebd87ca9b9a097fc48
Joshua Woodward <jdw6359@rit.edu>
2016-04-15 10:29:34 -0400

    Program Detail alert in student detail

--- app/controllers/student.js
+++ app/controllers/student.js
@@ -2,20 +2,15 @@
 
 StudentController.$inject = [
     'StudentFactory',
-    '$scope',
     '$routeParams'
 ];
 
-function StudentController(StudentFactory, $scope, $routeParams){
+function StudentController(StudentFactory, $routeParams){
 
     var vm = this;
     var student_id = $routeParams.id
     vm.student = StudentFactory.get({id: student_id});
     vm.transactions = StudentFactory.transactions({id: student_id});
-
-    $scope.refresh = function(){
-        vm.student = StudentFactory.get({id: $routeParams.id});
-    }
 }
 
 module.exports = StudentController;
